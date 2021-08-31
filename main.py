5ff219b4423b7f2b3fe3795bc52cc68b33aec490
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2021-08-31 15:46:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
@@ -20,19 +18,24 @@ async def on_ready():
     )
 
 svko = [
-    'Did you know I can terrorize men and women equally',
-    'I can tell',
-    'Stop pinging me',
-    'Use svko help if you need help (but not medical)',
-    'YES ITS ME',
-    'WHAT DO YOU WANT'
+    'did you know I can terrorize men and women equally',
+    'mhm',
+    'amazing',
+    'i can tell',
+    'stop pinging me',
+    'use svko help if you need help (but not medical)',
+    'yo',
+    '?',
+    '??',
+    'X D'
 ]
 
 svko_hi = [
-    'Hey',
-    'Wassup',
-    'Hello',
-    'Hi'
+    'hey',
+    'yo',
+    'wassup',
+    'hello',
+    'hi'
 ]
 
 help_str = """\
@@ -45,29 +48,33 @@ Available commands:
 
 @client.event
 async def on_message(message):
-    mention = f'<@!{client.user.id}>'
-
     if message.author == client.user:
         return
 
+    mention = f'<@!{client.user.id}>'
     if mention in message.content:
         response = random.choice(svko)
-        await message.channel.send(response)
-
-    if message.content.startswith("svko hi"):
-        response = random.choice(svko_hi)
-        await message.channel.send(response)
-
-    if message.content.startswith("svko help"):
-        await message.channel.send(help_str)
-
-    if message.content.startswith("svko image"):
-        image = os.listdir('responses/')
-        imgString = random.choice(image)
-        path = 'responses/' + imgString
         await message.channel.send(
-            "Random image from **revox's holy archive**:",
-            file=discord.File(path)
-        )
+            message.author.mention
+            + ", "
+            + response)
+
+    if message.content.startswith("svko"):
+        command = message.content.strip("svko ").split(" ")
+
+        if command[0].lower() in svko_hi:
+            response = random.choice(svko_hi)
+            await message.channel.send(response)
+
+        if command[0].lower() == "help":
+            await message.channel.send(help_str)
+
+        if command[0].lower() == "image":
+            image = os.listdir('responses/')
+            imgString = random.choice(image)
+            path = 'responses/' + imgString
+            await message.channel.send(
+                "Random image from **revox's holy archive**:",
+                file=discord.File(path))
 
 client.run(TOKEN)
